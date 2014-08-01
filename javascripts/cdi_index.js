32cdd15505b92afbbfd571af024bd47629aee8d7
Mila Frerichs <mila.frerichs@gmail.com>
2014-08-01 17:35:36 +0200

    Automated commit at 2014-08-01 15:35:36 UTC by middleman-deploy 0.2.4

--- javascripts/cdi_index.js
+++ javascripts/cdi_index.js
@@ -1,30 +1,106 @@
 (function() {
+  this.subIndex = function(data, countryName) {
+    var axisSubX, classes, country, indicators, margin, score, subHeight, subSvg, subX, subXAxis, subY, width, _i, _ref, _results;
+    if (countryName == null) {
+      countryName = 'Germany';
+    }
+    margin = {
+      top: 10,
+      right: 30,
+      bottom: 150,
+      left: 40
+    };
+    width = 900;
+    subHeight = 200;
+    classes = ['max-score', 'score'];
+    country = _.rest(_.values(_.findWhere(data, {
+      Country: countryName
+    })));
+    subY = d3.scale.linear().range([subHeight, 0]).domain([0, 10]);
+    subX = d3.scale.ordinal().rangeRoundBands([0, width], .1).domain((function() {
+      _results = [];
+      for (var _i = 0, _ref = country.length; 0 <= _ref ? _i < _ref : _i > _ref; 0 <= _ref ? _i++ : _i--){ _results.push(_i); }
+      return _results;
+    }).apply(this));
+    axisSubX = d3.scale.ordinal().rangeRoundBands([0, width], .1).domain(_.rest(_.keys(data[0])));
+    d3.select('.cdi-country-sub-index h2').text(countryName);
+    subSvg = d3.select('.cdi-country-sub-index svg').attr("width", width + margin.left + margin.right).attr("height", subHeight + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    indicators = subSvg.selectAll('g').data(country);
+    indicators.enter().append('g').attr('transform', function(d, i) {
+      return "translate(" + (subX(i)) + ",0)";
+    });
+    indicators.append('rect').attr('y', 0).attr('width', subX.rangeBand()).attr('height', subHeight).attr('class', classes[0]);
+    indicators.append('text').text(function(d) {
+      return d;
+    }).attr('y', function(d) {
+      return subY(d) - 10;
+    }).attr('x', subX.rangeBand() / 2).attr("text-anchor", "middle");
+    score = indicators.append('rect');
+    score.attr('y', function(d) {
+      return subY(d);
+    }).attr('width', subX.rangeBand()).attr('height', function(d, i) {
+      return subHeight - subY(d);
+    }).attr('class', function(d, i) {
+      return classes[1];
+    });
+    subXAxis = d3.svg.axis().scale(axisSubX).orient("bottom");
+    return subSvg.append("g").attr("class", "x axis").attr("transform", "translate(0," + subHeight + ")").call(subXAxis).selectAll("text").attr("y", 0).attr("x", 9).attr("dy", ".30em").attr("transform", "rotate(90)").style("text-anchor", "start");
+  };
+
   this.cdi_index = function(element, key) {
     if (key == null) {
-      key = 'overall';
+      key = 'Overall';
     }
-    return d3.json(rootPath + '/data/cdi.json', function(err, data) {
-      var bars, classes, dx, gesamt, height, heights, scale, svg, width;
-      gesamt = [10, data[key]];
+    return d3.csv(rootPath + '/data/cdi.csv', function(err, data) {
+      var classes, countries, dx, height, heights, margin, scale, score, subHeight, svg, width, x, xAxis, y;
+      data = _.sortBy(data, function(d) {
+        return d[key];
+      });
       heights = [40, 20];
       dx = [0, 10];
       classes = ['max-score', 'score'];
-      width = 300;
-      height = 60;
-      scale = d3.scale.linear().domain([0, d3.max(gesamt)]).range([0, width]);
-      svg = d3.select(element).append('svg').attr('width', width).attr('height', height);
-      bars = svg.selectAll('rect').data(gesamt);
-      bars.enter().append('rect');
-      bars.attr('x', 0).attr('y', function(d, i) {
-        return dx[i];
-      }).attr('width', function(d) {
-        return scale(d);
-      }).attr('height', function(d, i) {
-        return heights[i];
-      }).attr('class', function(d, i) {
-        return classes[i];
+      margin = {
+        top: 10,
+        right: 30,
+        bottom: 150,
+        left: 40
+      };
+      width = 900;
+      height = 300;
+      subHeight = 200;
+      scale = d3.scale.linear().domain([0, 10]).range([0, width]);
+      y = d3.scale.linear().range([height, 0]).domain([0, 10]);
+      x = d3.scale.ordinal().rangeRoundBands([0, width], .1).domain(data.map(function(d) {
+        return d.Country;
+      }));
+      svg = d3.select(element).append('svg').attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      countries = svg.selectAll('g').data(data);
+      countries.enter().append('g').attr('class', function(d) {
+        return d.Country;
+      }).attr('transform', function(d) {
+        return "translate(" + (x(d.Country)) + ",0)";
       });
-      return svg.append('text').text(data[key]).attr('x', scale(data[key]) + 10).attr('y', 25);
+      countries.append('rect').attr('y', 0).attr('width', x.rangeBand()).attr('height', height).attr('class', classes[0]);
+      score = countries.append('rect');
+      score.attr('y', function(d) {
+        return y(d[key]);
+      }).attr('width', x.rangeBand()).attr('height', function(d, i) {
+        return height - y(d[key]);
+      }).attr('class', function(d, i) {
+        return classes[1];
+      });
+      countries.append('text').text(function(d) {
+        return d[key];
+      }).attr('y', function(d) {
+        return y(d[key]) - 10;
+      }).attr('x', x.rangeBand() / 2).attr('text-anchor', 'middle');
+      xAxis = d3.svg.axis().scale(x).orient("bottom");
+      svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis).selectAll("text").attr("y", 0).attr("x", 9).attr("dy", ".30em").attr("transform", "rotate(90)").style("text-anchor", "start");
+      if (key === 'Overall') {
+        d3.select('.cdi-country-sub-index').append('h2').text('Germany');
+        d3.select('.cdi-country-sub-index').append('svg');
+        return subIndex(data);
+      }
     });
   };
 
--- javascripts/sicherheit.js
+++ javascripts/sicherheit.js
@@ -1,4 +1,30 @@
 (function() {
-  cdi_index('.cdi-index-sicherheit', 'sicherheit');
+  $(function() {
+    cdi_index('.cdi-index-sicherheit', 'Security');
+    $(".js-vertical-tab-content").hide();
+    $(".js-vertical-tab-content:first").show();
+    $(".js-vertical-tab").click(function(event) {
+      var activeTab;
+      event.preventDefault();
+      $(".js-vertical-tab-content").hide();
+      activeTab = $(this).attr("rel");
+      $("#" + activeTab).show();
+      $(".js-vertical-tab").removeClass("is-active");
+      $(this).addClass("is-active");
+      $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+      $(".js-vertical-tab-accordion-heading[rel^='" + activeTab + "']").addClass("is-active");
+    });
+    return $(".js-vertical-tab-accordion-heading").click(function(event) {
+      var accordion_activeTab;
+      event.preventDefault();
+      $(".js-vertical-tab-content").hide();
+      accordion_activeTab = $(this).attr("rel");
+      $("#" + accordion_activeTab).show();
+      $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+      $(this).addClass("is-active");
+      $(".js-vertical-tab").removeClass("is-active");
+      $(".js-vertical-tab[rel^='" + accordion_activeTab + "']").addClass("is-active");
+    });
+  });
 
 }).call(this);
