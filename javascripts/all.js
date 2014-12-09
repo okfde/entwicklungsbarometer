5e59a24cc16298fc970b2dbedb959bee6e588edd
Mila Frerichs <mila.frerichs@gmail.com>
2014-12-09 11:07:58 +0100

    Automated commit at 2014-12-09 10:07:58 UTC by middleman-deploy 0.2.4

--- javascripts/all.js
+++ javascripts/all.js
@@ -20626,7 +20626,7 @@ d3.numberFormat = locale_de_DE.numberFormat
         margin: {
           top: 40,
           right: 30,
-          bottom: 150,
+          bottom: 50,
           left: 80
         },
         ticks: {
@@ -20881,6 +20881,270 @@ d3.numberFormat = locale_de_DE.numberFormat
   $(function() {
     if ($('#sicherheit').length > 0) {
       return cdi_index('.cdi-index-sicherheit', 'Security');
+    }
+  });
+
+}).call(this);
+(function() {
+  var changeDiffClass, formatCurrency,
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+
+  formatCurrency = d3.numberFormat("$,.2f");
+
+  this.formatCountry = function(countryString) {
+    return countryString.replace(" ", "");
+  };
+
+  changeDiffClass = function(value) {
+    if (value !== 0) {
+      if (value > 0) {
+        return "positive";
+      } else {
+        return "negative";
+      }
+    }
+  };
+
+  $(function() {
+    var export2012_13Path, gesamt2013Path;
+    if ($('#tables').length > 0) {
+      export2012_13Path = rootPath + "/data/exporte_2012_2013.csv";
+      gesamt2013Path = rootPath + "/data/gesamt_exporte_2013.csv";
+      return queue().defer(d3.csv, export2012_13Path).defer(d3.csv, gesamt2013Path).await(function(error, data, gesamt) {
+        var exporte, gesamt2013, gesamtRuestung2013, margin, notFreeTable, options, ruestung2012, ruestung2013, ruestungExporte2013, tBody, tHeadTr, trs, width;
+        this.data = data;
+        ruestung2013 = _.filter(this.data, function(num) {
+          return num.ruestung > 0;
+        });
+        ruestung2013 = _.sortBy(ruestung2013, function(num) {
+          return -num.ruestung;
+        });
+        gesamtRuestung2013 = _.reduce(ruestung2013, (function(sum, d) {
+          return sum + parseInt(d.ruestung);
+        }), 0);
+        gesamt2013 = _.reduce(gesamt, (function(sum, d) {
+          return sum + parseInt(d.ruestung);
+        }), 0);
+        ruestung2012 = _.filter(this.data, function(num) {
+          var _ref;
+          return _ref = num.Country, __indexOf.call(ruestung2013.map(function(d) {
+            return d.Country;
+          }), _ref) >= 0;
+        });
+        ruestung2012 = _.sortBy(ruestung2012, function(num) {
+          return -num.ruestung;
+        });
+        d3.select('#ruestung .gesamt-ruestung').html(formatCurrency(gesamtRuestung2013));
+        d3.select('#ruestung .gesamt').html(formatCurrency(gesamt2013));
+        width = parseInt(d3.select('.ruestung-2013-chart').style('width'));
+        margin = {
+          top: 10,
+          right: 30,
+          bottom: 120,
+          left: 40
+        };
+        options = {
+          height: 300,
+          width: width - 20,
+          margin: margin
+        };
+        exporte = new Exporte(ruestung2013, options);
+        exporte.setYDomain([
+          0, d3.max(ruestung2013, function(d) {
+            return parseInt(d.ruestung);
+          })
+        ]);
+        exporte.setXDomain(ruestung2013.map(function(d) {
+          return d.Country;
+        }));
+        exporte.setDataKey();
+        exporte.render('.ruestung-2013-chart .chart');
+        $('.ruestung-2013-chart form input').change(function(e) {
+          if (this.value === '2012') {
+            exporte.setDataKey('Ruestung_2012');
+            exporte.setYDomain([
+              0, d3.max(ruestung2012, function(d) {
+                return parseInt(d.Ruestung_2012);
+              })
+            ]);
+            return exporte.update(ruestung2012);
+          } else {
+            exporte.setDataKey('ruestung');
+            exporte.setYDomain([
+              0, d3.max(ruestung2013, function(d) {
+                return parseInt(d.ruestung);
+              })
+            ]);
+            return exporte.update(ruestung2013);
+          }
+        });
+        ruestungExporte2013 = d3.select('#exporte-2013').append('table');
+        ruestungExporte2013.attr('class', 'table-borders');
+        tHeadTr = ruestungExporte2013.append('thead').append('tr');
+        tHeadTr.append('th').text('Land');
+        tHeadTr.append('th').text('2013');
+        tHeadTr.append('th').text('Differenz zu 2012');
+        tBody = ruestungExporte2013.append('tbody');
+        trs = tBody.selectAll('tr').data(ruestung2013);
+        trs.enter().append('tr').attr('class', function(d) {
+          return formatCountry(d.Country);
+        });
+        trs.append('td').text(function(d) {
+          return d.Country;
+        });
+        trs.append('td').text(function(d) {
+          return formatCurrency(d.ruestung);
+        });
+        trs.append('td').text(function(d) {
+          return formatCurrency(d.ruestung - d.Ruestung_2012);
+        }).attr('class', function(d) {
+          return changeDiffClass(d.ruestung - d.Ruestung_2012);
+        });
+        notFreeTable = d3.select('#all-exports').append('table');
+        notFreeTable.attr('class', 'table-borders');
+        tHeadTr = notFreeTable.append('thead').append('tr');
+        tHeadTr.append('th').text('Land');
+        tHeadTr.append('th').text('Exporte 2013');
+        tHeadTr.append('th').text('Rüstungsexporte 2013');
+        tHeadTr.append('th').text('Differenz Gesamtexporte zu 2012');
+        tHeadTr.append('th').text('Differenz Rüstungsexporte zu 2012');
+        tBody = notFreeTable.append('tbody');
+        trs = tBody.selectAll('tr').data(this.data);
+        trs.enter().append('tr');
+        trs.append('td').text(function(d) {
+          return d.Country;
+        });
+        trs.append('td').text(function(d) {
+          return formatCurrency(d.gesamt);
+        });
+        trs.append('td').text(function(d) {
+          return formatCurrency(d.ruestung);
+        });
+        trs.append('td').text(function(d) {
+          return formatCurrency(d.gesamt - d.Gesamt_2012);
+        }).attr('class', function(d) {
+          return changeDiffClass(d.gesamt - d.Gesamt_2012);
+        });
+        return trs.append('td').text(function(d) {
+          return formatCurrency(d.ruestung - d.Ruestung_2012);
+        }).attr('class', function(d) {
+          return changeDiffClass(d.ruestung - d.Ruestung_2012);
+        });
+      });
+    }
+  });
+
+}).call(this);
+(function() {
+  var ZivileFriedensfoerderung,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  ZivileFriedensfoerderung = (function(_super) {
+    __extends(ZivileFriedensfoerderung, _super);
+
+    function ZivileFriedensfoerderung(rawData, options) {
+      this.rawData = rawData;
+      this.options = options != null ? options : {};
+      this.mouseover = __bind(this.mouseover, this);
+      this.mouseout = __bind(this.mouseout, this);
+      this.options = _.defaults(this.options, {
+        width: 800,
+        height: 300,
+        margin: {
+          top: 40,
+          right: 80,
+          bottom: 50,
+          left: 80
+        },
+        ticks: {
+          y: 7,
+          x: 8
+        }
+      });
+    }
+
+    ZivileFriedensfoerderung.prototype.lineClassForElement = function(d) {
+      return d[0].Country.toLowerCase();
+    };
+
+    ZivileFriedensfoerderung.prototype.setScalesAndDomain = function(data) {
+      this.setDataKey('value');
+      this.setDateKey('year');
+      this.setYDomain([
+        0, d3.max(data, function(d) {
+          return d3.max(d, function(d) {
+            return parseInt(d.value);
+          });
+        })
+      ]);
+      this.setXDomain(d3.extent(data[0], (function(_this) {
+        return function(d) {
+          return _this.parseDateFromYear(d.year);
+        };
+      })(this)));
+      return this;
+    };
+
+    ZivileFriedensfoerderung.prototype.drawPersonal = function(countries) {
+      if (countries == null) {
+        countries = [];
+      }
+      this.data = _.filter(this.rawData, function(d) {
+        return _.contains(countries, d.Country);
+      });
+      this.data = _.groupBy(this.data, function(d) {
+        return d.Country;
+      });
+      this.data = _.map(this.data, (function(_this) {
+        return function(data) {
+          return _.sortBy(data, function(d) {
+            return _this.parseDateFromYear(d.year);
+          });
+        };
+      })(this));
+      this.setScalesAndDomain(this.data);
+      return this.data.forEach((function(_this) {
+        return function(d) {
+          return d.forEach(function(d) {
+            return d.year = _this.parseDateFromYear(d.year);
+          });
+        };
+      })(this));
+    };
+
+    ZivileFriedensfoerderung.prototype.dataFormat = function() {
+      return d3.numberFormat(",.");
+    };
+
+    ZivileFriedensfoerderung.prototype.mouseout = function(d) {
+      d3.select("." + (d.Country.toLowerCase()) + " path").classed("country-hover", false);
+      return this.focus.attr("transform", "translate(-100,-100)");
+    };
+
+    ZivileFriedensfoerderung.prototype.mouseover = function(d) {
+      d3.select("." + (d.Country.toLowerCase()) + " path").classed("country-hover", true);
+      this.focus.attr("transform", "translate(" + (this.xScale(d[this.dateKey])) + "," + (this.yScale(d[this.dataKey])) + ")");
+      return this.focus.select("text").text("" + d.Country + ": $" + (this.dataFormat()(d[this.dataKey])) + " Mio");
+    };
+
+    return ZivileFriedensfoerderung;
+
+  })(this.D3Linechart);
+
+  $(function() {
+    var zivilePath;
+    if ($('#friedensfoerderung .zivile').length > 0) {
+      zivilePath = "" + rootPath + "/data/security/friedensfoerderung/zivile.csv";
+      return d3.csv(zivilePath, function(data) {
+        var countries, personal;
+        countries = ['Austria', 'United States', 'Germany', 'United Kingdom', 'Norway'];
+        personal = new ZivileFriedensfoerderung(data);
+        personal.setLineClass('countries');
+        personal.drawPersonal(countries);
+        return personal.render('#friedensfoerderung .zivile');
+      });
     }
   });
 
--- javascripts/zivile-friedensf.js
+++ javascripts/zivile-friedensf.js
@@ -1,13 +1,13 @@
 (function() {
-  var PeacekeepingPersonal,
+  var ZivileFriedensfoerderung,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  PeacekeepingPersonal = (function(_super) {
-    __extends(PeacekeepingPersonal, _super);
+  ZivileFriedensfoerderung = (function(_super) {
+    __extends(ZivileFriedensfoerderung, _super);
 
-    function PeacekeepingPersonal(rawData, options) {
+    function ZivileFriedensfoerderung(rawData, options) {
       this.rawData = rawData;
       this.options = options != null ? options : {};
       this.mouseover = __bind(this.mouseover, this);
@@ -17,7 +17,7 @@
         height: 300,
         margin: {
           top: 40,
-          right: 30,
+          right: 80,
           bottom: 50,
           left: 80
         },
@@ -28,17 +28,17 @@
       });
     }
 
-    PeacekeepingPersonal.prototype.lineClassForElement = function(d) {
+    ZivileFriedensfoerderung.prototype.lineClassForElement = function(d) {
       return d[0].Country.toLowerCase();
     };
 
-    PeacekeepingPersonal.prototype.setScalesAndDomain = function(data) {
-      this.setDataKey('TotalPercent');
+    ZivileFriedensfoerderung.prototype.setScalesAndDomain = function(data) {
+      this.setDataKey('value');
       this.setDateKey('year');
       this.setYDomain([
         0, d3.max(data, function(d) {
           return d3.max(d, function(d) {
-            return d.TotalPercent;
+            return parseInt(d.value);
           });
         })
       ]);
@@ -50,7 +50,7 @@
       return this;
     };
 
-    PeacekeepingPersonal.prototype.drawPersonal = function(countries) {
+    ZivileFriedensfoerderung.prototype.drawPersonal = function(countries) {
       if (countries == null) {
         countries = [];
       }
@@ -67,13 +67,6 @@
           });
         };
       })(this));
-      this.data.forEach((function(_this) {
-        return function(d) {
-          return d.forEach(function(d) {
-            return d.TotalPercent = parseFloat(d.TotalCost) * 100;
-          });
-        };
-      })(this));
       this.setScalesAndDomain(this.data);
       return this.data.forEach((function(_this) {
         return function(d) {
@@ -84,36 +77,36 @@
       })(this));
     };
 
-    PeacekeepingPersonal.prototype.dataFormat = function() {
-      return d3.numberFormat(",.3f");
+    ZivileFriedensfoerderung.prototype.dataFormat = function() {
+      return d3.numberFormat(",.");
     };
 
-    PeacekeepingPersonal.prototype.mouseout = function(d) {
+    ZivileFriedensfoerderung.prototype.mouseout = function(d) {
       d3.select("." + (d.Country.toLowerCase()) + " path").classed("country-hover", false);
       return this.focus.attr("transform", "translate(-100,-100)");
     };
 
-    PeacekeepingPersonal.prototype.mouseover = function(d) {
+    ZivileFriedensfoerderung.prototype.mouseover = function(d) {
       d3.select("." + (d.Country.toLowerCase()) + " path").classed("country-hover", true);
       this.focus.attr("transform", "translate(" + (this.xScale(d[this.dateKey])) + "," + (this.yScale(d[this.dataKey])) + ")");
-      return this.focus.select("text").text("" + d.Country + ": " + (this.dataFormat()(d[this.dataKey])) + " % of GDP");
+      return this.focus.select("text").text("" + d.Country + ": $" + (this.dataFormat()(d[this.dataKey])) + " Mio");
     };
 
-    return PeacekeepingPersonal;
+    return ZivileFriedensfoerderung;
 
   })(this.D3Linechart);
 
   $(function() {
-    var personalPath;
-    if ($('#personal').length > 0) {
-      personalPath = "" + rootPath + "/data/security/peacekeeping/peacekeepingPersonal.csv";
-      return d3.csv(personalPath, function(data) {
+    var zivilePath;
+    if ($('#friedensfoerderung .zivile').length > 0) {
+      zivilePath = "" + rootPath + "/data/security/friedensfoerderung/zivile.csv";
+      return d3.csv(zivilePath, function(data) {
         var countries, personal;
-        countries = ['Austria', 'Finland', 'Germany', 'Luxembourg', 'EU', 'Slovakia'];
-        personal = new PeacekeepingPersonal(data);
+        countries = ['Austria', 'United States', 'Germany', 'United Kingdom', 'Norway'];
+        personal = new ZivileFriedensfoerderung(data);
         personal.setLineClass('countries');
         personal.drawPersonal(countries);
-        return personal.render('#personal');
+        return personal.render('#friedensfoerderung .zivile');
       });
     }
   });
